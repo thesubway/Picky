eae7809126590fc68465ba84e866d3df55325ef8
Tomo <tueda14@gmail.com>
2014-10-03 20:19:56 -0700

    Initial Commit

---
+++ Picky/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Picky
+//
+//  Created by Tomo Ueda on 10/3/14.
+//  Copyright (c) 2014 Tomo Ueda. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Picky/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Picky
+//
+//  Created by Tomo Ueda on 10/3/14.
+//  Copyright (c) 2014 Tomo Ueda. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
