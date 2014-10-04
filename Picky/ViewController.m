5d4251c4f7f8261ebe94c81b72e60eed2bd307df
William Wu <williamwu.2k12@gmail.com>
2014-10-03 21:26:13 -0700

    reverting

--- Picky/ViewController.m
+++ Picky/ViewController.m
@@ -16,14 +16,7 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    NSLog(@"some string 2");
     // Do any additional setup after loading the view, typically from a nib.
-}
-
-- (void) uselessMethod
-{
-    NSLog(@"go bears");
-    return;
 }
 
 - (void)didReceiveMemoryWarning {
