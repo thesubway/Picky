f7fd5463ff2b9b138cbb962ef3ebba78a76cb36f
William Wu <williamwu.2k12@gmail.com>
2014-10-04 02:03:05 -0700

    showing logan and luis

---
+++ Picky/Explore.m
@@ -0,0 +1,42 @@
+//
+//  Explore.m
+//  Picky
+//
+//  Created by William Wu on 10/4/14.
+//  Copyright (c) 2014 Tomo Ueda. All rights reserved.
+//
+
+#import "Explore.h"
+
+@interface Explore ()
+
+@end
+
+@implementation Explore
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+- (IBAction)printRandom:(id)sender {
+    NSLog(@"sdpafsdpfiauspfod");
+}
+
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
