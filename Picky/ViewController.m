a7fd331c584c7b49e48f214d39d7e2905ae8a32e
Logan Carter <logmac@berkeley.edu>
2014-10-03 23:38:30 -0700

    Changed what is printed

--- Picky/ViewController.m
+++ Picky/ViewController.m
@@ -16,7 +16,7 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    NSLog(@"some string");
+    NSLog(@"HI WILLIAM");
     // Do any additional setup after loading the view, typically from a nib.
 }
 
