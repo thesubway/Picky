224e4865409a243542fd4992833c8651cede30f2
Tomo <tueda14@gmail.com>
2014-10-03 21:07:12 -0700

    hello world

--- Picky/ViewController.m
+++ Picky/ViewController.m
@@ -17,7 +17,7 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     NSLog(@"facebook");
-    NSLOG(@"Quora");
+    NSLog(@"Quora");
     // Do any additional setup after loading the view, typically from a nib.
 }
 
