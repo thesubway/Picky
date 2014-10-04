4b94351bd26a6a883e2b6a8dc98af6a4569bff2f
William Wu <williamwu.2k12@gmail.com>
2014-10-03 21:06:56 -0700

    test merge

--- Picky/ViewController.m
+++ Picky/ViewController.m
@@ -17,7 +17,7 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     NSLog(@"facebook");
-    NSLOG(@"Quora");
+    NSLog(@"hello");
     // Do any additional setup after loading the view, typically from a nib.
 }
 
