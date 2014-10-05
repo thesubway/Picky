3c7677cf23ba291eda22cae256925b62ed042a58
Logan Carter <logmac@berkeley.edu>
2014-10-04 19:19:19 -0700

    improved meal class

--- Picky/Meal.m
+++ Picky/Meal.m
@@ -16,12 +16,34 @@
     {
         query = [PFQuery queryWithClassName:@"meal"];
         meal = [PFObject objectWithClassName:@"meal"];
+        numratings = 0;
+        avgrating = [[meal objectForKey:@"avgRating"] floatValue];
+        photos = [meal objectForKey:@"allPhotos"];
     }
     return self;
 }
 - (float) getAvgRating {
-    float rating = [[meal objectForKey:@"avgRating"] floatValue];
-    return rating;
+    return avgrating;
 }
 
+- (NSArray *) getPhotos {
+    return photos;
+}
+
+- (void) setAvgRating {
+    float sum = 0;
+    for (int i = 0; i < photos.count; i ++)
+    {
+        sum += [photos[i] getRetrievedRating]
+    }
+    float total = sum / numratings;
+    meal[@"avgRating"] = @total;
+    [meal saveInBackground];
+}
+
+- (void) setPhotos
+    {
+        //not sure how to do this yet, I think I need to use photo class, loop through all photos
+        //and add a photo to allPhotos if the name of the meal matches the name of this meal
+    }
 @end
