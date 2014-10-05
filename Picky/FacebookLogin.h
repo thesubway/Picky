2022595e5b4f93eb5d5b8f23b6eef3265f0d6bd6
Tomo <tueda14@gmail.com>
2014-10-05 05:11:26 -0700

    added facebook login capabilities.

---
+++ Picky/FacebookLogin.h
@@ -0,0 +1,14 @@
+//
+//  FacebookLogin.h
+//  Picky
+//
+//  Created by Tomo Ueda on 10/5/14.
+//  Copyright (c) 2014 Tomo Ueda. All rights reserved.
+//
+
+#import "ViewController.h"
+#import <FacebookSDK/FacebookSDK.h>
+
+@interface FacebookLogin : ViewController
+
+@end
